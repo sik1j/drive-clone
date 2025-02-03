d6cc3d44398f3405dceee11835cd83e57623b87c
sik1j <karkisikij2@gmail.com>
2025-02-03 13:55:20 -0800

    add: database connection and test schema

---
+++ drizzle.config.ts
@@ -0,0 +1,17 @@
+import { type Config } from "drizzle-kit";
+
+import { env } from "~/env";
+
+export default {
+  schema: "./src/server/db/schema.ts",
+  dialect: "singlestore",
+  tablesFilter: ["drive_tutorial_*"],
+  dbCredentials: {
+    host: env.SINGLESTORE_HOST,
+    port: parseInt(env.SINGLESTORE_PORT),
+    user: env.SINGLESTORE_USER,
+    password: env.SINGLESTORE_PASS,
+    database: env.SINGLESTORE_DB_NAME,
+    ssl: {},
+  },
+} satisfies Config;
---
+++ src/server/db/schema.ts
@@ -0,0 +1,12 @@
+import {
+  int,
+  bigint,
+  text,
+  singlestoreTable,
+} from "drizzle-orm/singlestore-core";
+
+export const users = singlestoreTable("users_table", {
+  id: bigint("id", { mode: "bigint" }).primaryKey().autoincrement(),
+  name: text("name"),
+  age: int("age"),
+});
