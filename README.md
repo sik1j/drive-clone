ffd6d063435a9fb5fec8e577ac815f2e83626007
sik1j <karkisikij2@gmail.com>
2025-02-03 18:53:35 -0800

    impl: database connection

--- README.md
+++ README.md
@@ -6,3 +6,11 @@
 - [ ] Move folder open state (breadcrumbs) to URL
 - [ ] Add auth
 - [ ] Add file uploading
+
+### Note 03-02-2025
+
+Finished connecting database. Next steps:
+- [ ] Update schema to show files and folders
+- [ ] Manually insert examples
+- [ ] Render them in the UI
+- [ ] Push and make sure it all works
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,17 @@
-import { type Config } from "drizzle-kit";
+import { defineConfig } from "drizzle-kit";
 
 import { env } from "~/env";
 
-export default {
+export default defineConfig({
   schema: "./src/server/db/schema.ts",
-  dialect: "sqlite",
-  dbCredentials: {
-    url: env.DATABASE_URL,
-  },
+  dialect: "singlestore",
   tablesFilter: ["drive-tutorial_*"],
-} satisfies Config;
+  dbCredentials: {
+    host: env.SINGLESTORE_HOST,
+    port: parseInt(env.SINGLESTORE_PORT),
+    user: env.SINGLESTORE_USER,
+    password: env.SINGLESTORE_PASS,
+    database: env.SINGLESTORE_DB_NAME,
+    ssl: {},
+  },
+});
